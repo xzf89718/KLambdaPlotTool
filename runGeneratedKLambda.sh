191b2c3ceaa338552f3117fdbedd2dec6e4b50c9
zifeng.xu <mg20220214@smail.nju.edu.cn>
2021-03-04 23:38:27 +0800

    before merge, add debug config

--- runGeneratedKLambda.sh
+++ runGeneratedKLambda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-list_KLambda="3.0 5.0 -20.0 -10.0 -5.0 -1.0 0.0"
+list_KLambda="3.0 5.0 -20.0 -10.0 -5.0 -1.0 0.0 15.0 -15.0 12.0 10.0 "
 for KLambda in $list_KLambda
 do
         echo "now we are woking on Klambda= "$KLambda
