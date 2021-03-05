3917449cde7601399429081e094aa8ef7ee08db2
zifeng <mg20220214@smail.nju.edu.cn>
2021-03-05 14:07:09 +0800

    DEBUG

--- GenerateKLambaSamples.C
+++ GenerateKLambaSamples.C
@@ -10,7 +10,7 @@
 #include <string>
 #include <map>
 #include <cmath>
-
+#include <fstream>
 #include "TROOT.h"
 #include "TFile.h"
 #include "TTree.h"
@@ -42,6 +42,8 @@ void GenerateKLambaSamples(const double KLambda)
         if(index_of_point != -1)
                 string_KLambda.replace(index_of_point, 1, "p");
         //for now, we only need 2 digit decimal
+        //string_KLambda = string_KLambda.substr(0, string_KLambda.length() - 4);
+        // now, we noly need 1 digit decimal
         string_KLambda = string_KLambda.substr(0, string_KLambda.length() - 4);
         //if KLambda<0, we need to substitute the "-" with "n"
         auto index_of_minus = string_KLambda.find("-");
@@ -94,83 +96,83 @@ void GenerateKLambaSamples(const double KLambda)
 
         // Add base_sample name
         // Here we use KL 1p0 10p0 generated and KL 20p0 reweighted from 10p0 
-    // (now maybe we can switch to reweigted from 1p0 in the next step!)
-    base_names.insert(std::pair<std::string, std::string>("base", "hhttbbKL"));
-    base_names.insert(std::pair<std::string, std::string>("1p0", "hhttbbKL1p0"));
-    base_names.insert(std::pair<std::string, std::string>("10p0", "hhttbbKL10p0"));
-    // base_names.push_back("hhttbbKL20p0from1p0");
-    base_names.insert(std::pair<std::string, std::string>("20p0", "hhttbbKL20p0from10p0"));
+        // (now maybe we can switch to reweigted from 1p0 in the next step!)
+        base_names.insert(std::pair<std::string, std::string>("base", "hhttbbKL"));
+        base_names.insert(std::pair<std::string, std::string>("1p0", "hhttbbKL1p0"));
+        base_names.insert(std::pair<std::string, std::string>("10p0", "hhttbbKL10p0"));
+        // base_names.push_back("hhttbbKL20p0from1p0");
+        base_names.insert(std::pair<std::string, std::string>("20p0", "hhttbbKL20p0from10p0"));
 
-    // HERE_IS_BASE_FILE
-    auto base_file = new TFile("../output/KLReweight_py8.root", "READ");
-    if (!base_file)
-    {
-        cerr << "error: " << "Can't open basefile, Check HERE_IS_BASE!" << endl;
-        return;
-    }
-    auto output_file = new TFile("../output/KLRecoReweigt_py8.root", "UPDATE");
-    if (!output_file)
-    {
-        cerr << "error: " << "Can't open outputfile, Check HERE_IS_BASE!" << endl;
-        return;
-    }
-    // write base infomation into the output_file
-    double base1 = 1;
-    double base2 = 10;
-    double base3 = 20;
-    auto base_info = new TTree("base_info", "base_info");
-    base_info->Branch("base1", &base1, "base1/D");
-    base_info->Branch("base2", &base2, "base2/D");
-    base_info->Branch("base3", &base3, "base3/D");
-    base_info->Fill();
-    output_file->Write("base_info");
-
-    // Now only consider Preselection
-    // vector<std::string> Dir_names;
-    std::string dirName("Preselection");
-    auto dir_Preselection = (TDirectory*)base_file->Get(dirName.c_str());
-    if (!dir_Preselection)
-    {
-        cerr << "error: " << "Can't open TDirectory, Check dirName!" << endl;
-        return;
-    }
-
-    // Create Preselection directory in output_file and write histogram
-    if (!output_file->Get(dirName.c_str()))
-    {
-        output_file->mkdir(dirName.c_str());
-        clog << "Warning: Cant find Preselection in output file, mkdir!" << endl;
-    }
-    //
-    output_file->cd(dirName.c_str());
-
-    // start to combine the histogram and writte ! 
-    for (auto iter_variable = variable_names.begin(); iter_variable != variable_names.end(); iter_variable ++)
-    {       
-        for(auto iter_region = region_names.begin(); iter_region != region_names.end(); iter_region ++)
+        // HERE_IS_BASE_FILE
+        auto base_file = new TFile("./output/KLReweight_py8.root", "READ");
+        if (!base_file)
         {
-            // Reweighted histogram name
-            auto hist_name_reweighted = base_names.at("base") + string_KLambda + "fromReco" + \
-                                        "_" + *iter_region + "_" + *iter_variable;
-            // Get three base histogram
-            auto h1_name = base_names.at("1p0") + "_" + *iter_region + "_" + *iter_variable;
-            auto h10_name = base_names.at("10p0") + "_" + *iter_region + "_" + *iter_variable;
-            auto h20_name = base_names.at("20p0") + "_" + *iter_region + "_" + *iter_variable;
+                cerr << "error: " << "Can't open basefile, Check HERE_IS_BASE!" << endl;
+                return;
+        }
+        auto output_file = new TFile("./output/KLRecoReweigt_py8.root", "UPDATE");
+        if (!output_file)
+        {
+                cerr << "error: " << "Can't open outputfile, Check HERE_IS_BASE!" << endl;
+                return;
+        }
+        // write base infomation into the output_file
+        double base1 = 1;
+        double base2 = 10;
+        double base3 = 20;
+        auto base_info = new TTree("base_info", "base_info");
+        base_info->Branch("base1", &base1, "base1/D");
+        base_info->Branch("base2", &base2, "base2/D");
+        base_info->Branch("base3", &base3, "base3/D");
+        base_info->Fill();
+        output_file->Write("base_info");
 
-            auto h1 = (TH1F*)dir_Preselection->Get(h1_name.c_str());
-            auto h10 = (TH1F*)dir_Preselection->Get(h10_name.c_str());
-            auto h20 = (TH1F*)dir_Preselection->Get(h20_name.c_str());
+        // Now only consider Preselection
+        // vector<std::string> Dir_names;
+        std::string dirName("Preselection");
+        auto dir_Preselection = (TDirectory*)base_file->Get(dirName.c_str());
+        if (!dir_Preselection)
+        {
+                cerr << "error: " << "Can't open TDirectory, Check dirName!" << endl;
+                return;
+        }
 
-            if(!h1)
-            {
-                clog << "Warning: Cant find base histogram h0" << endl;
-            }
-            if(!h10)
-            {
-                clog << "Warning: Cant find base histogram h1" << endl;
-            }
-            if(!h20)
-            {
+        // Create Preselection directory in output_file and write histogram
+        if (!output_file->Get(dirName.c_str()))
+        {
+                output_file->mkdir(dirName.c_str());
+                clog << "Warning: Cant find Preselection in output file, mkdir!" << endl;
+        }
+        //
+        output_file->cd(dirName.c_str());
+
+        // start to combine the histogram and writte ! 
+        for (auto iter_variable = variable_names.begin(); iter_variable != variable_names.end(); iter_variable ++)
+        {       
+                for(auto iter_region = region_names.begin(); iter_region != region_names.end(); iter_region ++)
+                {
+                        // Reweighted histogram name
+                        auto hist_name_reweighted = base_names.at("base") + string_KLambda + "fromReco" + \
+                                                    "_" + *iter_region + "_" + *iter_variable;
+                        // Get three base histogram
+                        auto h1_name = base_names.at("1p0") + "_" + *iter_region + "_" + *iter_variable;
+                        auto h10_name = base_names.at("10p0") + "_" + *iter_region + "_" + *iter_variable;
+                        auto h20_name = base_names.at("20p0") + "_" + *iter_region + "_" + *iter_variable;
+
+                        auto h1 = (TH1F*)dir_Preselection->Get(h1_name.c_str());
+                        auto h10 = (TH1F*)dir_Preselection->Get(h10_name.c_str());
+                        auto h20 = (TH1F*)dir_Preselection->Get(h20_name.c_str());
+
+                        if(!h1)
+                        {
+                                clog << "Warning: Cant find base histogram h0" << endl;
+                        }
+                        if(!h10)
+                        {
+                                clog << "Warning: Cant find base histogram h1" << endl;
+                        }
+                        if(!h20)
+                        {
                                 clog << "Warning: Cant find base histogram h10" << endl;
                         }
                         if(!h1 || !h10 || !h20)
@@ -183,7 +185,7 @@ void GenerateKLambaSamples(const double KLambda)
                         delete h1;
                         delete h10;
                         delete h20;
-                        
+
                         h1 = (TH1F*)dir_Preselection->Get(h1_name.c_str())->Clone();
                         h10 = (TH1F*)dir_Preselection->Get(h10_name.c_str())->Clone();
                         h20 = (TH1F*)dir_Preselection->Get(h20_name.c_str())->Clone();
@@ -192,22 +194,30 @@ void GenerateKLambaSamples(const double KLambda)
                         // Methods to obtain signal templates for HH signals with couplings variations
                         // We take KT = 1 here
                         //
-//                        auto h_cup = ( 200. / 171. - KLambda * 10. / 57. + pow(KLambda, 2) * 1 / 171) * (*h1)\
-//                                     + ((-2. / 9.0) + KLambda * 7. / 30. - pow(KLambda, 2) * 1. / 90. )* (*h10) \
-//                                     + (1. / 19. - KLambda * 11. / 190. + pow(KLambda, 2) * 1. / 190. ) * (*h20);
-//                        h_cup.SetName(hist_name_reweighted.c_str());
+                        //                        auto h_cup = ( 200. / 171. - KLambda * 10. / 57. + pow(KLambda, 2) * 1 / 171) * (*h1)\
+                        //                                     + ((-2. / 9.0) + KLambda * 7. / 30. - pow(KLambda, 2) * 1. / 90. )* (*h10) \
+                        //                                     + (1. / 19. - KLambda * 11. / 190. + pow(KLambda, 2) * 1. / 190. ) * (*h20);
+                        //                        h_cup.SetName(hist_name_reweighted.c_str());
                         auto h_cup = new TH1F(*h1);
+                        // here are the 3 coefficient
+                        double k1 = 200. / 171. - KLambda * 10. / 57. + pow(KLambda, 2) * 1 / 171;
+                        double k2 = (-2. / 9.0) + KLambda * 7. / 30. - pow(KLambda, 2) * 1. / 90.;
+                        double k3 = 1. / 19. - KLambda * 11. / 190. + pow(KLambda, 2) * 1. / 190.;
                         h_cup->Sumw2();
                         h_cup->Add(h1, h10, \
-                                200. / 171. - KLambda * 10. / 57. + pow(KLambda, 2) * 1 / 171, \
-                                (-2. / 9.0) + KLambda * 7. / 30. - pow(KLambda, 2) * 1. / 90.);
+                                        k1,\
+                                        k2);
                         auto h_cup_2 = new TH1F(*h_cup);
                         h_cup_2->Sumw2();
                         h_cup_2->Add(h_cup_2, h20,\
-                                    1,
-                                    1. / 19. - KLambda * 11. / 190. + pow(KLambda, 2) * 1. / 190.);
+                                        1,
+                                        k3);
                         h_cup_2->SetName(hist_name_reweighted.c_str());
 #ifdef DEBUG_KLREWEIGT
+                        std::ofstream debug_k;
+                        debug_k.open("./output/All_k.txt",std::ios::app);
+                        debug_k << "KLambda: " <<KLambda <<" k1: " << k1<<" k2: " << k2<<" k3: " <<k3 <<endl;
+                        debug_k.close();
                         auto c1 = new TCanvas("c1", "c1");
                         h1->Draw();
                         h10->Draw("SAME");
@@ -215,11 +225,11 @@ void GenerateKLambaSamples(const double KLambda)
                         h_cup_2->Draw("SAME");
                         h1->SetLineColor(kRed);
                         h10->SetLineColor(kGreen);
-                        h20->SetLineColor(kRed + 10);
-                        h_cup_2->SetLineColor(kGreen + 10);
+                        h20->SetLineColor(kRed + 2);
+                        h_cup_2->SetLineColor(kGreen + 2);
                         c1->BuildLegend();
-                        c1->SetLogy();
-                        c1->SaveAs(("../output/plots/" + hist_name_reweighted + ".png").c_str());
+                       // c1->SetLogy();
+                        c1->SaveAs(("./output/plots/" + hist_name_reweighted + ".pdf").c_str());
                         cout << "hist_name_reweighted: " << hist_name_reweighted << endl;
                         delete c1;
 #endif
