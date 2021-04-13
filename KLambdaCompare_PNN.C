69fe2ec6f23239761d57d904f9c54019d6427132
zifeng <mg20220214@smail.nju.edu.cn>
2021-04-13 16:26:25 +0800

    Try to refactor KLCompare.C

--- KLambdaCompare_PNN.C
+++ KLambdaCompare_PNN.C
@@ -61,6 +61,130 @@ std::string transformFloat(float KLambda){
 
 }
 
+void DrawRatioPlot(TH1F* h1, TH1F* h2){
+    //cout<< h1->GetNbinsX() <<endl;
+    h1->Rebin(rebin_factors.at(*iter_variable));
+    h2->Rebin(rebin_factors.at(*iter_variable));
+    // Define the Canvas
+    TCanvas *c = new TCanvas("c", "canvas", 800, 800);
+    gROOT->SetStyle("ATLAS");
+    gStyle->SetOptStat(0);
+    // Upper plot will be in pad1
+    TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
+    pad1->SetBottomMargin(1); // Upper and lower plot are joined
+    // pad1->SetGridx();         // Vertical grid
+    pad1->Draw();             // Draw the upper pad: pad1
+    pad1->cd();               // pad1 becomes the current pad
+    h1->SetStats(0);          // No statistics on upper plot
+    h1->Draw("HIST E1");               // Draw h1
+    h2->Draw("HIST E1 SAME");         // Draw h2 on top of h1
+    // Create Legend for histogram!
+    //auto legend = new TLegend(0.7, 0.9, 0.48, 0.9);
+    auto legend = new TLegend(0.75, 0.8, 0.95, 0.95);
+    gStyle->SetLegendTextSize(0.02);
+    legend->AddEntry(h1, "truth_level");
+    legend->AddEntry(h2, "combine_at_reco_level");
+    legend->Draw();
+
+
+    // Do not draw the Y axis label on the upper plot and redraw a small
+    // axis instead, in order to avoid the first label (0) to be clipped.
+    //h1->GetYaxis()->SetLabelSize(0.);
+    // TGaxis *axis = new TGaxis( -5, 20, -5, 220, 20,220,510,"");
+    // axis->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+    // axis->SetLabelSize(15);
+    // axis->Draw();
+
+    // lower plot will be in pad
+    c->cd();          // Go back to the main canvas before defining pad2
+    TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
+    pad2->SetTopMargin(0);
+    pad2->SetBottomMargin(0.2);
+    //pad2->SetGridx(); // vertical grid
+    pad2->SetGridy(); // horizontal grid
+    h1->GetXaxis()->SetTitle("");
+    h1->GetYaxis()->SetTitle("Events");
+    pad2->Draw();
+    pad2->cd();       // pad2 becomes the current pad
+
+    // Define the ratio plot
+    TH1F *h3 = (TH1F*)h1->Clone("h3");
+    h3->SetLineColor(kBlack);
+    h3->SetMinimum(0.8);  // Define Y ..
+    h3->SetMaximum(1.35); // .. range
+    h3->Sumw2();
+    h3->SetStats(0);      // No statistics on lower plot
+    h3->Divide(h2);
+    h3->SetMarkerStyle(21);
+    h3->Draw("ep");       // Draw the ratio plot
+
+    // h1 settings
+    h1->SetLineColor(kBlue+1);
+    h1->SetLineWidth(2);
+
+    // Y axis h1 plot settings
+    h1->GetYaxis()->SetTitleSize(20);
+    h1->GetYaxis()->SetTitleFont(43);
+    h1->GetYaxis()->SetTitleOffset(1.55);
+
+    // h2 settings
+    h2->SetLineColor(kRed);
+    h2->SetLineWidth(2);
+
+    // Ratio plot (h3) settings
+    h3->SetTitle(""); // Remove the ratio title
+
+    // Y axis ratio plot settings
+    h3->GetYaxis()->SetTitle("ratio h1/h2 ");
+    h3->GetYaxis()->SetNdivisions(505);
+    h3->GetYaxis()->SetTitleSize(20);
+    h3->GetYaxis()->SetTitleFont(43);
+    h3->GetYaxis()->SetTitleOffset(1.55);
+    h3->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+    h3->GetYaxis()->SetLabelSize(15);
+
+    // X axis ratio plot settings
+    if(*iter_variable == "mHH" || *iter_variable == "mMMC" || *iter_variable == "mHH")
+    {
+        h3->GetXaxis()->SetTitle((*iter_variable + "[GeV]").c_str());
+    }
+    else
+    {
+        h3->GetXaxis()->SetTitle((*iter_variable).c_str());
+    }
+    h3->GetXaxis()->SetTitleSize(20);
+    h3->GetXaxis()->SetTitleFont(43);
+    h3->GetXaxis()->SetTitleOffset(4.);
+    h3->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+    h3->GetXaxis()->SetLabelSize(15);
+
+    gPad->Update();
+    c->SaveAs((output_path + "/" + hist_name + ".png").c_str());
+    delete h1;
+    delete h2;
+    delete h3;
+    delete pad1;
+    delete pad2;
+
+    delete c;
+
+}
+
+std::vector<std::string>* GetRegions(){
+}
+
+std::vector<std::string>* GetVariables(){
+}
+
+std::map<std::string, int>* GetRebinFactors(){
+}
+
+std::vector<std::string>* GetBaseNames(){
+}
+
+std::map<std::string, std::string>* GetRecoBaseName(){
+}
+
 void KLambdaCompare( ) {
     // Define two gaussian histograms. Note the X and Y title are defined
     // at booking time using the convention "Hist_title ; X_title ; Y_title"
@@ -68,8 +192,7 @@ void KLambdaCompare( ) {
     // TH1F *h2 = new TH1F("h2", "h2", 100, -5, 5);
     // h1->FillRandom("gaus");
     // h2->FillRandom("gaus");
-    //
-    
+
 
     //std::string Dir = {"Preselection"};
     std::string input_dir = {"BDTScorePreselection"};
@@ -78,11 +201,11 @@ void KLambdaCompare( ) {
     auto KLReweight_Reco = new TFile("./output/KLRecoReweigt_py8.root", "READ");
     std::string output_path = {"./plotsForGenerateKL"};
     // Initialize STL containers, wich includes names of histograms
-    std::vector<std::string> regions;
-    std::vector<std::string> variables;
-    std::map<std::string, int> rebin_factors;
-    std::vector<std::string> base_names;
-    std::map<std::string, std::string> Reco_Basename;
+    std::vector<std::string>* regions;
+    std::vector<std::string>* variables;
+    std::map<std::string, int>* rebin_factors;
+    std::vector<std::string>* base_names;
+    std::map<std::string, std::string>* Reco_Basename;
 
     // Add regions
     regions.push_back("2tag2pjet_0ptv_LL_OS");
@@ -214,111 +337,7 @@ void KLambdaCompare( ) {
                 auto h1 = (TH1F*)dir_py8->Get(hist_name.c_str())->Clone();
                 //auto h2 = (TH1F*)dir_herwig7->Get(hist_name.c_str())->Clone();
                 auto h2 = (TH1F*)dir_Reco->Get(hist_name_reco.c_str())->Clone();
-                //cout<< h1->GetNbinsX() <<endl;
-                h1->Rebin(rebin_factors.at(*iter_variable));
-                h2->Rebin(rebin_factors.at(*iter_variable));
-                // Define the Canvas
-                TCanvas *c = new TCanvas("c", "canvas", 800, 800);
-                gROOT->SetStyle("ATLAS");
-                gStyle->SetOptStat(0);
-                // Upper plot will be in pad1
-                TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
-                pad1->SetBottomMargin(1); // Upper and lower plot are joined
-                // pad1->SetGridx();         // Vertical grid
-                pad1->Draw();             // Draw the upper pad: pad1
-                pad1->cd();               // pad1 becomes the current pad
-                h1->SetStats(0);          // No statistics on upper plot
-                h1->Draw("HIST E1");               // Draw h1
-                h2->Draw("HIST E1 SAME");         // Draw h2 on top of h1
-                // Create Legend for histogram!
-                //auto legend = new TLegend(0.7, 0.9, 0.48, 0.9);
-                auto legend = new TLegend(0.75, 0.8, 0.95, 0.95);
-                gStyle->SetLegendTextSize(0.02);
-                legend->AddEntry(h1, "truth_level");
-                legend->AddEntry(h2, "combine_at_reco_level");
-                legend->Draw();
-
-
-                // Do not draw the Y axis label on the upper plot and redraw a small
-                // axis instead, in order to avoid the first label (0) to be clipped.
-                //h1->GetYaxis()->SetLabelSize(0.);
-                // TGaxis *axis = new TGaxis( -5, 20, -5, 220, 20,220,510,"");
-                // axis->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-                // axis->SetLabelSize(15);
-                // axis->Draw();
-
-                // lower plot will be in pad
-                c->cd();          // Go back to the main canvas before defining pad2
-                TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
-                pad2->SetTopMargin(0);
-                pad2->SetBottomMargin(0.2);
-                //pad2->SetGridx(); // vertical grid
-                pad2->SetGridy(); // horizontal grid
-                h1->GetXaxis()->SetTitle("");
-                h1->GetYaxis()->SetTitle("Events");
-                pad2->Draw();
-                pad2->cd();       // pad2 becomes the current pad
-
-                // Define the ratio plot
-                TH1F *h3 = (TH1F*)h1->Clone("h3");
-                h3->SetLineColor(kBlack);
-                h3->SetMinimum(0.8);  // Define Y ..
-                h3->SetMaximum(1.35); // .. range
-                h3->Sumw2();
-                h3->SetStats(0);      // No statistics on lower plot
-                h3->Divide(h2);
-                h3->SetMarkerStyle(21);
-                h3->Draw("ep");       // Draw the ratio plot
-
-                // h1 settings
-                h1->SetLineColor(kBlue+1);
-                h1->SetLineWidth(2);
-
-                // Y axis h1 plot settings
-                h1->GetYaxis()->SetTitleSize(20);
-                h1->GetYaxis()->SetTitleFont(43);
-                h1->GetYaxis()->SetTitleOffset(1.55);
-
-                // h2 settings
-                h2->SetLineColor(kRed);
-                h2->SetLineWidth(2);
-
-                // Ratio plot (h3) settings
-                h3->SetTitle(""); // Remove the ratio title
-
-                // Y axis ratio plot settings
-                h3->GetYaxis()->SetTitle("ratio h1/h2 ");
-                h3->GetYaxis()->SetNdivisions(505);
-                h3->GetYaxis()->SetTitleSize(20);
-                h3->GetYaxis()->SetTitleFont(43);
-                h3->GetYaxis()->SetTitleOffset(1.55);
-                h3->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-                h3->GetYaxis()->SetLabelSize(15);
-
-                // X axis ratio plot settings
-                if(*iter_variable == "mHH" || *iter_variable == "mMMC" || *iter_variable == "mHH")
-                {
-                    h3->GetXaxis()->SetTitle((*iter_variable + "[GeV]").c_str());
-                }
-                else
-                {
-                    h3->GetXaxis()->SetTitle((*iter_variable).c_str());
-                }
-                h3->GetXaxis()->SetTitleSize(20);
-                h3->GetXaxis()->SetTitleFont(43);
-                h3->GetXaxis()->SetTitleOffset(4.);
-                h3->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-                h3->GetXaxis()->SetLabelSize(15);
-
-                gPad->Update();
-                c->SaveAs((output_path + "/" + hist_name + ".png").c_str());
-                delete h1;
-                delete h2;
-                delete h3;
-                delete pad1;
-                delete pad2;
-
-                delete c;
+                DrawRatioPlot(h1, h2);
             }
         }
     }
--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ mkdir plots
 mkdir output
 mkdir ./output/plots
 
+echo "Make sure you have already installed ROOT!"
+echo $ROOTSYS
